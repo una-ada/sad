bde57f2bd8342e5f62a3cdffa608c6580d27d96c
Una Hoglund <trewbot@gmail.com>
2022-05-30 21:46:17 -0700

    infra: npm i cannon-es

--- src/Physics.ts
+++ src/Physics.ts
@@ -1,4 +1,42 @@
-import { Intersection, Raycaster, Vector3 } from 'three';
+/**
+ * @file Sad Physics Wrapper
+ * @author Una Ada <una@xn--z7x.dev>
+ * @version 2.0.1
+ * 
+ * Physics in the [original version]{@link https://xn--z7x.dev/sad/v2017} were
+ * manually written using the THREE.js ray-caster. For extensibility and 
+ * reduction of scope in this project, I've decided to switch to using a 
+ * pre-built physics engine.
+ * 
+ * Rewriting the physics by hand was also considered, referencing the THREE.js
+ * [FPS example]{@link https://threejs.org/examples/#games_fps} and using the
+ * octree implementation provided therein; however, this would again limit the 
+ * extensibility as well as generally be a pain on my part.
+ * 
+ * The initial pick for physics engines was a JS port of the Bullet engine
+ * [ammo.js]{@link https://github.com/kripken/ammo.js} due to THREE.js
+ * [examples]{@link https://threejs.org/examples/#physics_ammo_instancing}
+ * featuring it. This idea was shelved due to complications with my attempt to
+ * implement it and the subsequent difficulty of bugfixes due to a lack of
+ * available documentation.
+ * 
+ * [OimoPhysics]{@link https://github.com/saharan/OimoPhysics} was also 
+ * considered for similar reasons. This one has available documentation
+ * ({@link https://saharan.github.io/OimoPhysics/oimo/}), though it is clearly
+ * lacking. My decision to move on from it was based on some performance tests
+ * compared to ammo.js as well as its limited API.
+ * 
+ * Ultimately, I decided on a natively written (rather than ported) engine:
+ * [Cannon]{@link https://github.com/schteppe/cannon.js}. This was chosen based
+ * on the [source code]{@link https://github.com/brunosimon/folio-2019} of 
+ * Bruno Simon's [portfolio]{@link https://bruno-simon.com/}, an impressive use 
+ * of THREE.js. The documentation for this is unavailable, as is its homepage, 
+ * as they original creator seems to have moved on; however, the developer 
+ * collective [Poimandres]{@link http://pmnd.rs/} actively maintains an updated
+ * [fork]{@link https://github.com/pmndrs/cannon-es}, which is what I am using 
+ * in the wrapper here. Notably, this branch also removes the necessity for an
+ * additional types dependency.
+ */
 import { Level } from './levels/Level';
 import { Player } from './Player';
 
@@ -11,28 +49,8 @@ export class Physics {
     this.level = level;
     this.rate = rate;
   }
-  getPlayerGrounds(): Intersection[] {
-    let caster = new Raycaster(
-      this.player.position.clone(),
-      new Vector3(0, -1, 0),
-      0,
-      this.player.data.step + 0.01
-    );
-    caster.ray.origin.y -= this.player.data.height - this.player.data.step;
-    return caster.intersectObjects(this.level.children);
-  }
   update = (): void => {
-    console.log(this.player.velocity, this.player.canJump);
-    this.player.velocity.y -= 0.98 / this.rate;
-    var grounds = this.getPlayerGrounds();
-    if (grounds.length > 0) {
-      this.player.position.y = grounds[0].point.y + this.player.data.height;
-      this.player.velocity.y = Math.max(this.player.velocity.y, 0);
-      this.player.canJump = true;
-    } else {
-      this.player.canJump = false;
-    }
-    this.player.position.add(this.player.velocity);
+
   };
   loop = (): void => {
     this.update();
--- src/Player.ts
+++ src/Player.ts
@@ -1,14 +1,16 @@
 import { PerspectiveCamera, Vector3 } from 'three';
+import { Capsule } from 'three/examples/jsm/math/Capsule';
 import { PlayerData } from './PlayerData';
 
 /**
- * The Player class creates a player in the level as an extension of a camera 
+ * The Player class creates a player in the level as an extension of a camera
  * as this is a first person experience.
  */
 export class Player extends PerspectiveCamera {
   public data: PlayerData;
-  public canJump: boolean;
+  public floor: boolean;
   public velocity: Vector3;
+  public hitBox: Capsule;
   constructor() {
     let aspect = window.innerWidth / window.innerHeight;
     super(75, aspect, 0.1, 1e3);
@@ -21,12 +23,19 @@ export class Player extends PerspectiveCamera {
     };
     this.position.set(2, this.data.height, 4);
     this.rotation.set(0, 0, 0);
+
+    this.hitBox = new Capsule(
+      new Vector3(0, 0.35, 0),
+      new Vector3(0, 1, 0),
+      0.35
+    );
+
     this.velocity = new Vector3();
-    this.canJump = true;
+    this.floor = true;
   }
-  
-  handleResize = ():void => {
+
+  handleResize = (): void => {
     this.aspect = window.innerWidth / window.innerHeight;
     this.updateProjectionMatrix();
-  }
+  };
 }
